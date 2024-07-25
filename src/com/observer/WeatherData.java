0ffd483a16d0a2eb6f15e41d44c6af51f9f821bf
Kavindu <kavinduinduwara100@gmail.com>
2024-07-25 22:12:14 +0530

    Oberver pattern added - not working yet

---
+++ src/com/observer/WeatherData.java
@@ -0,0 +1,45 @@
+package com.observer;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+    private ArrayList observers;
+    private float temperature;
+    private float pressure;
+    private float humidity;
+
+    public WeatherData(){
+        observers = new ArrayList();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver (Observer o){
+        int i = observers.indexOf(o);
+        if (i >= 0){
+            observers.remove(i);
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (int i = 0; i < observers.size(); i++){
+            Observer observer = (Observer)observers.get(i);
+            observer.update(temperature,humidity,pressure);
+        }
+    }
+
+    public void measurementsChanged(){
+        notifyObservers();
+    }
+
+    public void setMeasurements( float temperature, float humidity, float pressure){
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.temperature = temperature;
+    }
+
+}
